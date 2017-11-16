989524ac2d9a632536a9578dead37c17f6d75846
Matt Power <matthewallenpower@gmail.com>
2017-11-16 10:16:17 -0500

    Add in file uploader.

--- sketch.js
+++ sketch.js
@@ -1,15 +1,28 @@
+var input;
 var img;
+var bg;
 
 function setup() {
-  createCanvas(612, 792);
-  background('white');
-
-  img = loadImage('images/template.png');
+  var c = createCanvas(612, 792);
+  background('pink');
+  bg = loadImage('images/template.png');
+  // uploadImg = loadImage('images/bauhaus.png');
+  input = createFileInput(handleFile);
+  input.position(35, 35);
 }
 
 function draw() {
-  var uploadImg = loadImage('images/bauhaus.png');
-  image(img, 0, 0, 613, 794);
-  image(uploadImg, 40, 40, 50, 50);
+  // image(uploadImg, 189, 71, 360, 360);
+  // image(img, 0, 0, 613, 794);
+  // image(bg, 0, 0, width, height);
+  rect(mouseX, mouseY, 1, 1);
+}
 
+function handleFile(file) {
+  print(file);
+  if (file.type == 'image') {
+    img = createImg(file.data).addClass('lit');
+    img.hide();
+    image(img, 0, 0, width, height);
+  }
 }
