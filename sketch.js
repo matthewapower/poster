c40d57ec129c653f0b92349967b17f3d54bb6fc1
Matt Power <matthewallenpower@gmail.com>
2017-11-17 15:49:04 -0500

    add in text functionality .

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@ var img;
 var bg;
 var input;
 var button;
-var swag;
+var textInput;
+var userText;
 
 function setup() {
   var c = createCanvas(612, 792);
@@ -12,6 +13,11 @@ function setup() {
   button = createButton('save');
   button.position(35, 55);
   button.mousePressed(saveCanvas);
+  textInput = createInput();
+  textInput.position(35, 75);
+  textInput.changed(addText);
+  userText = false;
+  background('#ececec');
   bg = loadImage('images/template.png', function(bg){
     image(bg, 0, 0, 613, 794);
   });
@@ -20,13 +26,23 @@ function setup() {
 function draw() {
 }
 
-function processImg(upload) {
+function processImg(upload, callback) {
   $(function(){
     $(upload).duotone({ gradientMap: '#df343c, #ececec' });
-    img = $(upload).attr('src');
+    callback(upload);
+  });
+}
 
-    loadImage(img, function(img){
-      image(img, 189, 71, 360, 360);
+function addImg(upload) {
+  $(function(){
+    var processedSrc;
+    processedSrc = $(upload).attr('src');
+    background('#ececec');
+    loadImage(processedSrc, function(processedSrc){
+      image(processedSrc, 189, 71, 360, 360);
+    });
+    loadImage('images/template.png', function(bg){
+      image(bg, 0, 0, 613, 794);
     });
   });
 }
@@ -34,15 +50,38 @@ function processImg(upload) {
 function gotFile(f) {
   if (f.type === 'image') {
     var size = f.size;
-    img = createImg(f.data).addClass(size);
-    processImg('.' + size);
+    img = createImg(f.data).addClass(size).hide();
+    processImg('.' + size, addImg);
   } else {
     print(`"${f.name}" isn't an image file!`);
   }
 }
 
 function mouseDragged() {
-	strokeWeight(5);
-  stroke('#df343c');
-	line(mouseX, mouseY, pmouseX, pmouseY);
+  if (userText === false ) {
+    strokeWeight(5);
+    stroke('#df343c');
+  	line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+
+function mouseMoved() {
+  if (textInput.value().length > 0) {
+    userText.position(winMouseX, winMouseY);
+  }
+}
+
+function mousePressed() {
+  if (userText !== false ) {
+    noStroke();
+    textSize(45);
+    text(userText.elt.textContent, mouseX, mouseY);
+    userText = false;
+    $('.user-text').remove();
+  }
+}
+
+function addText() {
+  noStroke();
+  userText = createP(textInput.value()).addClass('user-text');
 }
