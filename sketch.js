2138c42d5e567c8e6e4014eac9663195cd31fd58
Matt Power <matthewallenpower@gmail.com>
2017-11-17 09:37:39 -0500

    Add in image colorization.

--- sketch.js
+++ sketch.js
@@ -2,33 +2,47 @@ var img;
 var bg;
 var input;
 var button;
+var swag;
 
 function setup() {
   var c = createCanvas(612, 792);
-  background('pink');
-  bg = loadImage('images/template.png');
   c.drop(gotFile);
   input = createFileInput(gotFile);
   input.position(35, 35);
   button = createButton('save');
   button.position(35, 55);
   button.mousePressed(saveCanvas);
+  bg = loadImage('images/template.png', function(bg){
+    image(bg, 0, 0, 613, 794);
+  });
 }
 
 function draw() {
-  if (img) {
-    image(img, 189, 71, 360, 360);
-  } else {
-    text('Drag an image file onto the canvas.', width>>1, height>>1);
-  }
-  image(bg, 0, 0, 613, 794);
-  rect(mouseX, mouseY, 1, 1);
+}
+
+function processImg(upload) {
+  $(function(){
+    $(upload).duotone({ gradientMap: '#df343c, #ececec' });
+    img = $(upload).attr('src');
+
+    loadImage(img, function(img){
+      image(img, 189, 71, 360, 360);
+    });
+  });
 }
 
 function gotFile(f) {
   if (f.type === 'image') {
-    img = loadImage(f.data, redraw);
+    var size = f.size;
+    img = createImg(f.data).addClass(size);
+    processImg('.' + size);
   } else {
     print(`"${f.name}" isn't an image file!`);
   }
+}
+
+function mouseDragged() {
+	strokeWeight(5);
+  stroke('#df343c');
+	line(mouseX, mouseY, pmouseX, pmouseY);
 }
